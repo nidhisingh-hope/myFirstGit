7c2d9778ca7ec23582d808b6123f471890eebabb
Nidhi Singh <nidhi.singh.31@outlook.com>
2019-10-20 12:42:58 +0000

    Made code tolerable

--- calc.py
+++ calc.py
@@ -2,18 +2,14 @@ def add(x,y)
 x+y
 
 def sub(x,y)
-<<<<<<< HEAD
 return x-y
-=======
-pass
->>>>>>> 3735958b580778d32830431ac377a680e8b471ea
+
 
 def mul(x,y)
 pass
 
 def div(x,y)
 return x/y
-<<<<<<< HEAD
-=======
 
->>>>>>> 3735958b580778d32830431ac377a680e8b471ea
+def square(x,y)
+pass
